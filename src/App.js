e9e21d567575d6c7e9e4e4868b1ee416079d16c8
indigo <diego97523@gmail.com>
2023-08-01 05:49:41 +0900

    0801_KaKaoLogin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Main_page from "./pages/Main_page";
+import Login_page from "./pages/Login_page";
 function App() {
   return (
-    <div className="App">
-    </div>
+      <Login_page/>
   );
 }
 
---
+++ src/pages/Login_page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import KakaoLogin from '../components/KakaoLogin';
+const Login_page = () => {
+    return (
+        <KakaoLogin/>
+    );
+};
+
+export default Login_page;
