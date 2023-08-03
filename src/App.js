9aa57ddfb22150cd4c0b94f682bae10b0f824f46
indigo <diego97523@gmail.com>
2023-08-04 00:46:47 +0900

    0804

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Main_page from "./pages/Main_page";
 import Login_page from "./pages/Login_page";
+import Feed from "./pages/Feed";
 function App() {
   return (
-      <Login_page/>
+     <Feed/>
   );
 }
 
