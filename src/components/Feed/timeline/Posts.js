a7d90acef9825926151089c0a0a646ca9b1e4ad0
chae401 <chae401@sju.ac.kr>
2023-08-12 05:15:43 +0900

    [feat] feed

--- src/components/Feed/timeline/Posts.js
+++ src/components/Feed/timeline/Posts.js
@@ -1,27 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../../../css/Post.module.css";
 import Avatar from "@mui/material/Avatar";
 import Rating from "@mui/material/Rating";
 import HeartOutLineIcon from "@mui/icons-material/FavoriteBorder";
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import StarIcon from '@mui/icons-material/Star';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
-const CustomRating = ({ value }) => {
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+import moment from "moment/moment";
+
+const CustomRating = (props) => {
+  console.log("score", props.score);
   return (
     <Rating
       name="custom-rating"
-      value={3.5}
+      value={props.score}
       precision={0.5}
       //realOnly
       emptyIcon={<StarIcon style={{ opacity: 0.5 }} />}
-      icon={<StarIcon style={{ color: 'rgba(255, 0, 0, 0.6)' }} />}
-      halfIcon={<StarHalfIcon style={{ color: 'rgba(255, 0, 0, 0.6)' }} />}
+      icon={<StarIcon style={{ color: "rgba(255, 0, 0, 0.6)" }} />}
+      halfIcon={<StarHalfIcon style={{ color: "rgba(255, 0, 0, 0.6)" }} />}
     />
   );
 };
+const getDayMinuteCounter = (props) => {
+  if (!props.date) {
+    return "";
+  }
+  const today = moment();
+  const postingDate = moment(props.date);
+  const dayDiff = postingDate.diff(today, "days");
+  const hourDiff = postingDate.diff(today, "hours");
+  const minutesDiff = postingDate.diff(today, "minutes");
 
-const Posts = ({ user, postImage, likes, timeStamp, chats }) => {
+  if (dayDiff === 0 && hourDiff === 0) {
+    // 작성한지 1시간도 안지났을때
+    const minutes = Math.ceil(-minutesDiff);
+    return minutes + "분 전"; // '분' 로 표시
+  }
+
+  if (dayDiff === 0 && hourDiff <= 24) {
+    // 작성한지 1시간은 넘었지만 하루는 안지났을때,
+    const hour = Math.ceil(-hourDiff);
+    return hour + "시간 전"; // '시간'으로 표시
+  }
+  console.log(-dayDiff + "일 전");
+  return -dayDiff + "일 전";
+};
+
+const Posts = ({
+  boardId,
+  userId,
+  writer,
+  storeId,
+  storeName,
+  content,
+  score,
+  likes,
+  imgUrlList,
+  replyList,
+  createdDate,
+}) => {
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(likes);
 
@@ -39,25 +78,27 @@ const Posts = ({ user, postImage, likes, timeStamp, chats }) => {
       <div className={styles.post}>
         <div className={styles.post_header}>
           <div className={styles.postheader_user}>
-            <Avatar className={styles.avatar}>{user.charAt(0).toUpperCase()}</Avatar>
-            {user} <span>_{timeStamp}</span>
+            <Avatar className={styles.avatar}>
+              {writer.charAt(0).toUpperCase()}
+            </Avatar>
+            {writer} <span>{getDayMinuteCounter({ date: createdDate })}</span>
           </div>
-          <CustomRating/>
+          <CustomRating score={score} />
         </div>
         <div className={styles.post_image}>
-          <img src={postImage} alt='image' />
+          <img src={imgUrlList[0]} alt="image" />
         </div>
         <div className={styles.footer}>
           <div className={styles.post_footerIcons}>
             <div className={styles.Icons_union}>
               <HeartOutLineIcon
                 className={styles.postIcon1}
-                style={{ color: isLiked ? 'red' : 'inherit' }}
+                style={{ color: isLiked ? "red" : "inherit" }}
                 onClick={handleLikeClick}
               />
-              <span className={styles.like}>{likeCount}</span> 
+              <span className={styles.like}>{likes}</span>
               <ChatBubbleOutlineIcon className={styles.postIcon2} />
-              <span className={styles.chat}>{chats}</span>
+              <span className={styles.chat}>{replyList.length}</span>
             </div>
             <div className={styles.Icons_save}>
               <BookmarkBorderIcon className={styles.postIcon3} />
--- src/components/Feed/timeline/Timeline.js
+++ src/components/Feed/timeline/Timeline.js
@@ -1,70 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../../css/Timeline.module.css";
 import Posts from "./Posts";
 import BottomBar from "../../BottomBar";
 import SearchBar from "../../SearchBar";
+import axios from "axios";
 const Timeline = () => {
-  const [posts, setPost] = useState([
-    {
-      user: "KimBeomSu",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
-      likes: 42312,
-      timeStamp: "3일전",
-      chats: 100,
-    },
-    {
-      user: "KangChangJin",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
-      likes: 112345,
-      timeStamp: "30분전",
-      chats: 200,
-    },
-    {
-      user: "KimChaeWon",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
-      likes: 451234,
-      timeStamp: "1일전",
-      chats: 400,
-    },
-    {
-      user: "LeeChangGyu",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
-      likes: 82839,
-      timeStamp: "10일전",
-      chats: 300,
-    },
-    {
-      user: "KimSuMin",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
-      likes: 12345,
-      timeStamp: "3일전",
-      chats: 500,
-    },
-    {
-      user: "KimMinGi",
-      postImage:
-        "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
-      likes: 42124,
-      timeStamp: "1분전",
-      chats: 600,
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  const getPostList = async () => {
+    const resp = await (
+      await axios.get("http://54.180.53.205/board/viewAll")
+    ).data; // 2) feed data 할당
+    setPosts(resp); // posts에 data 할당
+    console.log("resp", resp);
+  };
+
+  useEffect(() => {
+    getPostList(); // 1) feed 조회 함수 호출
+  }, []);
+  // const [posts, setPost] = useState([
+  //   {
+  //     user: "KimBeomSu",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
+  //     likes: 42312,
+  //     timeStamp: "3일전",
+  //     chats: 100,
+  //   },
+  //   {
+  //     user: "KangChangJin",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
+  //     likes: 112345,
+  //     timeStamp: "30분전",
+  //     chats: 200,
+  //   },
+  //   {
+  //     user: "KimChaeWon",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
+  //     likes: 451234,
+  //     timeStamp: "1일전",
+  //     chats: 400,
+  //   },
+  //   {
+  //     user: "LeeChangGyu",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
+  //     likes: 82839,
+  //     timeStamp: "10일전",
+  //     chats: 300,
+  //   },
+  //   {
+  //     user: "KimSuMin",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/32cba5e4-4d70-488c-a246-1ff2bdff8094.jpg",
+  //     likes: 12345,
+  //     timeStamp: "3일전",
+  //     chats: 500,
+  //   },
+  //   {
+  //     user: "KimMinGi",
+  //     postImage:
+  //       "https://s3.ap-northeast-2.amazonaws.com/markeybucket/post/image/45637da7-6253-4ea5-ae1c-4754c8f3345c.jpg",
+  //     likes: 42124,
+  //     timeStamp: "1분전",
+  //     chats: 600,
+  //   },
+  // ]);
   return (
     <div className={styles.container}>
       <div className={styles.timeline}>
         <div className={styles.timeline_posts}>
           {posts.map((post) => (
             <Posts
-              user={post.user}
-              postImage={post.postImage}
+              // user={post.user}
+              // postImage={post.postImage}
+              // likes={post.likes}
+              // timeStamp={post.timeStamp}
+              // chats={post.chats}
+              boardId={post.boardId}
+              userId={post.userId}
+              writer={post.writer}
+              storeId={post.storeId}
+              storeName={post.storeName}
+              content={post.content}
+              score={post.score}
               likes={post.likes}
-              timeStamp={post.timeStamp}
-              chats={post.chats}
+              imgUrlList={post.imgUrlList}
+              replyList={post.replyList}
+              createdDate={post.createdDate}
             />
           ))}
         </div>
