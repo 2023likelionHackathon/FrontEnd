c06312cb405f932a64e9df8d95c04a41ac6dad03
chae401 <chae401@sju.ac.kr>
2023-08-16 17:50:55 +0900

    [feat] rating Value 추가

--- src/components/Sijang_info/Sijang_detail/Sijang_detail4.js
+++ src/components/Sijang_info/Sijang_detail/Sijang_detail4.js
@@ -13,23 +13,23 @@ const Sijang_detail4 = () => {
         <div className={styles.title_star}>
           <div className={styles.title}>떡마을</div>
           <Rating
-          className={styles.Rating}
-          name="star_rating"
-          precision={0.1}
-          value={4.0} //임시값임 서버에서  받아와서 들어갈값 
-          readOnly
-          emptyIcon={<StarIcon style={{ opacity: 0.6, fontSize: "30px" }} />}
-          icon={
-            <StarIcon
-              style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
-            />
-          }
-          halfIcon={
-            <StarHalfIcon
-              style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
-            />
-          }
-        ></Rating>
+            className={styles.Rating}
+            name="star_rating"
+            precision={0.1}
+            value={4.0} //임시값임 서버에서  받아와서 들어갈값
+            readOnly
+            emptyIcon={<StarIcon style={{ opacity: 0.6, fontSize: "30px" }} />}
+            icon={
+              <StarIcon
+                style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
+              />
+            }
+            halfIcon={
+              <StarHalfIcon
+                style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
+              />
+            }
+          ></Rating>
         </div>
         <div className={styles.loc}>
           뚝도시장 | 떡집 <br />
