8fcff7afbea250af075348c1296e4bd6e8fd62c7
chae401 <chae401@sju.ac.kr>
2023-08-16 15:54:04 +0900

    [feat] 점포 관련 피드 연결

--- src/pages/Jumpo_show3.js
+++ src/pages/Jumpo_show3.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import Sijang_detail3 from "../components/Sijang_info/Sijang_detail/Sijang_detail3";
 import BottomBar from "../components/BottomBar";
+import axios from "axios";
 
 const Jumpo_show3 = () => {
+  const [posts, setPosts] = useState();
+  const [store, setStore] = useState();
+  const getPostList = async () => {
+    const resp = await axios.get("http://api.domarketdodo.shop/store/view/2");
+    setPosts(resp.data.boardList); // posts에 data 할당
+    setStore(resp.data.store);
+    console.log("resp", resp.data);
+  };
   return (
     <div>
       <SearchBar />
-      <Sijang_detail3 />
+      <Sijang_detail3 posts={posts} store={store} />
       <BottomBar />
     </div>
   );
