eea8f15b09e408b47879e41686173f70a330b11c
chae401 <chae401@sju.ac.kr>
2023-08-18 00:25:41 +0900

    [fix] pwd->pw

--- src/components/About_Login/Join_Membership.js
+++ src/components/About_Login/Join_Membership.js
@@ -23,8 +23,8 @@ const Join_Membership = () => {
   const handlePasswordCheck = () => {
     //비번 확인 비교 함수
     setPasswordMatch(formData.pw === formData.pwd_check);
-    console.log("pwd", formData.pw);
-    console.log("pwd_check", formData.pwd_check);
+    //console.log("pwd", formData.pw);
+    //console.log("pwd_check", formData.pwd_check);
   };
   const handleDuplicateCheckId = async (checkValue) => {
     //서버로 중복확인 요청 보내고 결과 받는 함수
@@ -57,9 +57,7 @@ const Join_Membership = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData.pwd);
-    console.log(formData.pwd_check);
-    if (formData.pwd !== formData.pwd_check) {
+    if (formData.pw !== formData.pwd_check) {
       alert("비밀번호가 일치하지 않습니다.");
       return;
     }
