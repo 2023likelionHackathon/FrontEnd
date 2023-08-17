60f8d7de94ba154c615a79f6fe7ceaf4b5000ac9
chae401 <chae401@sju.ac.kr>
2023-08-17 15:48:30 +0900

    edit

--- src/components/Sijang_info/StoreDetail.js
+++ src/components/Sijang_info/StoreDetail.js
@@ -8,6 +8,8 @@ import { useParams } from "react-router-dom";
 import { markets } from "../../lib/market";
 import Timeline from "../Feed/timeline/Timeline";
 import axios from "axios";
+import BottomBar from "../BottomBar";
+import SearchBar from "../SearchBar";
 
 const StoreDetail = () => {
   const [posts, setPosts] = useState([]);
@@ -32,101 +34,107 @@ const StoreDetail = () => {
 
   console.log(store.category);
   return (
-    <div className={styles.container}>
-      <div className={styles.intro}>
-        <img src={store.image} className={styles.image} />
-        <div className={styles.title_star}>
-          <div className={styles.title}>{store.name}</div>
-          <Rating
-            className={styles.Rating}
-            name="star_rating"
-            precision={0.2}
-            value={jumpo != null ? jumpo.score : 3} //임시값임 서버에서  받아와서 들어갈값
-            readOnly
-            emptyIcon={<StarIcon style={{ opacity: 0.6, fontSize: "30px" }} />}
-            icon={
-              <StarIcon
-                style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
-              />
-            }
-            halfIcon={
-              <StarHalfIcon
-                style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
-              />
-            }
-          ></Rating>
-        </div>
+    <div>
+      <SearchBar />
+      <div className={styles.container}>
+        <div className={styles.intro}>
+          <img src={store.image} className={styles.image} />
+          <div className={styles.title_star}>
+            <div className={styles.title}>{store.name}</div>
+            <Rating
+              className={styles.Rating}
+              name="star_rating"
+              precision={0.2}
+              value={jumpo != null ? jumpo.score : 3} //임시값임 서버에서  받아와서 들어갈값
+              readOnly
+              emptyIcon={
+                <StarIcon style={{ opacity: 0.6, fontSize: "30px" }} />
+              }
+              icon={
+                <StarIcon
+                  style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
+                />
+              }
+              halfIcon={
+                <StarHalfIcon
+                  style={{ color: " rgba(247, 128, 128)", fontSize: "30px" }}
+                />
+              }
+            ></Rating>
+          </div>
 
-        <div className={styles.loc}>
-          {market.name} | {store.category} <br />
-          {store.address}
-        </div>
-        <hr />
-        <div className={styles.open_time}>
-          <div className={styles.time}>영업시간</div>
-          <div className={styles.field}>
-            <div className={styles.days}>
-              {store.times.map((time) => {
-                return <div className={styles.day}>{time.day}</div>;
-              })}
+          <div className={styles.loc}>
+            {market.name} | {store.category} <br />
+            {store.address}
+          </div>
+          <hr />
+          <div className={styles.open_time}>
+            <div className={styles.time}>영업시간</div>
+            <div className={styles.field}>
+              <div className={styles.days}>
+                {store.times.map((time) => {
+                  return <div className={styles.day}>{time.day}</div>;
+                })}
+              </div>
+              <div className={styles.lines}>
+                {store.times.map((t) => {
+                  return (
+                    <div className={styles.line}>
+                      <img src="/pics/Vector39.png" className={styles.line__} />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className={styles.use_}>
+                {store.times.map((t) => {
+                  return <div className={styles.uses_}>{t.time}</div>;
+                })}
+              </div>
             </div>
-            <div className={styles.lines}>
-              {store.times.map((t) => {
-                return (
+          </div>
+          <hr />
+          <div className={styles.menu}>
+            <div className={styles.samechu}>
+              <div className={styles.samechu_}>사메추</div>
+              <FavoriteIcon color="error" style={{ fontSize: 32 }} />
+              <div className={styles.samechu_text}>
+                사장님이 직접 추천하는 메뉴
+              </div>
+            </div>
+            {store.menu.map((m) => {
+              return (
+                <div className={styles.menu_img_text}>
+                  <img src={m.img} className={styles.gazami_img} />
+                  <div className={styles.menu_text}>
+                    <span className={styles.title_menu}>{m.name}</span>
+                    <div className={styles.price}>{m.price}</div>
+                    <div className={styles.explain}>{m.description}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+          <div className={styles.menus_}>
+            <div className={styles.menu_info}>메뉴정보</div>
+            {store.submenu?.map((m) => {
+              return (
+                <div className={styles.menu3}>
+                  <div className={styles.food}>{m.name}</div>
                   <div className={styles.line}>
                     <img src="/pics/Vector39.png" className={styles.line__} />
                   </div>
-                );
-              })}
-            </div>
-            <div className={styles.use_}>
-              {store.times.map((t) => {
-                return <div className={styles.uses_}>{t.time}</div>;
-              })}
-            </div>
-          </div>
-        </div>
-        <hr />
-        <div className={styles.menu}>
-          <div className={styles.samechu}>
-            <div className={styles.samechu_}>사메추</div>
-            <FavoriteIcon color="error" style={{ fontSize: 32 }} />
-            <div className={styles.samechu_text}>
-              사장님이 직접 추천하는 메뉴
-            </div>
-          </div>
-          {store.menu.map((m) => {
-            return (
-              <div className={styles.menu_img_text}>
-                <img src={m.img} className={styles.gazami_img} />
-                <div className={styles.menu_text}>
-                  <span className={styles.title_menu}>{m.name}</span>
                   <div className={styles.price}>{m.price}</div>
-                  <div className={styles.explain}>{m.description}</div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <hr />
+          <Timeline posts={posts}></Timeline>
         </div>
-        <hr />
-        <div className={styles.menus_}>
-          <div className={styles.menu_info}>메뉴정보</div>
-          {store.submenu?.map((m) => {
-            return (
-              <div className={styles.menu3}>
-                <div className={styles.food}>{m.name}</div>
-                <div className={styles.line}>
-                  <img src="/pics/Vector39.png" className={styles.line__} />
-                </div>
-                <div className={styles.price}>{m.price}</div>
-              </div>
-            );
-          })}
-        </div>
-        <hr />
-        <Timeline posts={posts}></Timeline>
+        <div className={styles.margin_div}></div>
       </div>
-      <div className={styles.margin_div}></div>
+      <BottomBar index={"시장 검색"} />
     </div>
   );
 };
